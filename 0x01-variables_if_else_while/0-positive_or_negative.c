00d7f4e3a9e70fbd48f425c7afb0e699b62b2fa4
daniel jemmo <danieljemmo1@gmail.com>
2023-06-16 02:38:39 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,14 +10,14 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if(n>0){
-		printf("%d is positive\n", n)
+	if(n<0){
+		printf("%d is negative\n", n);
 	}
 	else if(n==0){
-		pritf("%d: is zero\n", n)
+		pritf("%d: is zero\n", n);
 	}
-	else (n<0){
-	printf("%d: is negattive\n", n)
+	else {
+	printf("%d is positive\n", n);
 	}	
 	return (0);
 }
