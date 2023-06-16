ad189f383a36da1656b4ab0c980077673c3deb29
daniel jemmo <danieljemmo1@gmail.com>
2023-06-16 12:42:30 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (sucess);
+ */
+int main(void)
+{
+	char c;
+
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num * '0');
+	}
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
