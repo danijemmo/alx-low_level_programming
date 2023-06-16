758a5376644ebe9a08c77553533372066acdcd9f
daniel jemmo <danieljemmo1@gmail.com>
2023-06-16 12:28:24 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include<stido.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num + '0');
+	}
+	putchar('\n');
+	return (0);
+}
