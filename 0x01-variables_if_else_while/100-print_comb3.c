5b3d0737623c6ea8123bceda6fc16bea5253e0df
daniel jemmo <danieljemmo1@gmail.com>
2023-06-16 13:06:44 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (sucess);
+ */
+int main(void)
+{
+	int ones;
+	int tens;
+
+	for (tens = 0; tens <= 9; tens++)
+	{
+		for (ones = tens + 1 ; ones <= 9; ones++)
+		{
+			putchar(tens + '0');
+			putchar(ones + '0');
+			if (ones < 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
