5224c3ba83e0a1a3b091c5ab1a3398939cc24903
daniel jemmo <danieljemmo1@gmail.com>
2023-06-16 10:56:01 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
 
 /**
- *main - Entry point 
- * 
- *  Return: Always 0 (sucess); 
- */ 
+ *main - Entry point
+ *
+ *Return: Always 0 (sucess);
+ */
 int main(void)
 {
 	char alphabet;
+
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	{
 		putchar(alphabet);
