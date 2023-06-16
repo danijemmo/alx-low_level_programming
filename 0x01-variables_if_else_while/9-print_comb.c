111eba34dea38f7b57991c414e2a5033e94c104d
daniel jemmo <danieljemmo1@gmail.com>
2023-06-16 12:55:02 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,8 +11,11 @@ int main(void)
 	for (num = 0; num < 10; num++)
 	{
 		putchar(num + '0');
-		putchar(',');
-		putchar(' ');
+		if (num <9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
