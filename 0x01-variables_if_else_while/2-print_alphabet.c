0e95377406d83334bd1c26871d0e23fe75e764b8
daniel jemmo <danieljemmo1@gmail.com>
2023-06-16 10:26:40 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,10 +4,11 @@
  *
  * Return: Always 0 (sucess);
  */
-int main (void)
+int main(void)
 {
 	char alphabet;
-	for(alphabet = 'A';alphabet <= 'Z';alphabet++)
+
+	for (alphabet = 'a';alphabet <= 'z';alphabet++)
 	{
 		putchar(alphabet);
 	}
