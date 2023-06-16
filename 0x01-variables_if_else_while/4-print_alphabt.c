95d84121470a1404daf9809863effae4f8d6a6ad
daniel jemmo <danieljemmo1@gmail.com>
2023-06-16 11:14:43 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,16 +6,15 @@
  */
 int main(void)
 {
-        char alphabet;
-
-        for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-        {
+	char alphabet;
+	
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
 		if(alphabet != 'e' && alphabet != 'q')
 		{
 			putchar(alphabet);
 		}
-        }
-        putchar('\n');
-
-        return (0);
+	}
+	putchar('\n');
+	return (0);
 }
