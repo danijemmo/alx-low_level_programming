b0ae11918dcd2c7b6125b240fe363487ce9ec3a1
daniel jemmo <danieljemmo1@gmail.com>
2023-06-16 11:12:01 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (sucess);
+ */
+int main(void)
+{
+        char alphabet;
+
+        for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+        {
+		if(alphabet != 'e' && alphabet != 'q')
+		{
+			putchar(alphabet);
+		}
+        }
+        putchar('\n');
+
+        return (0);
+}
