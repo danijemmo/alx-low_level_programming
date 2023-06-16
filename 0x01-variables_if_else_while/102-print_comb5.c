110ab586fa7cb51f8f1374d900996e7e6fbf02e9
daniel jemmo <danieljemmo1@gmail.com>
2023-06-16 13:44:01 -0700

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,20 +9,23 @@ int main(void)
 	int i;
 	int j;
 
-	for (i = 0; i <= 97; i++)
+	for (i = 0; i <= 98; i++)
 	{
-		for (j = i + 1; j <= 98; j++)
+		for (j = i + 1; j <= 99; j++)
 		{
-			putchar((i / 10) + '0');
-			putchar((i % 10) + '0');
-			putchar(' ');
-			putchar((j / 10) + '0');
-			putchar((j % 10) + '0');
-
-			if (i < 97)
+			if (i < j)
 			{
-				putchar(',');
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
 				putchar(' ');
+				putchar((j / 10) + '0');
+				putchar((j % 10) + '0');
+
+				if (i < 97)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
