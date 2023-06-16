a7568de07f7a6a45b71ce3861e9243f0ad04c95a
daniel jemmo <danieljemmo1@gmail.com>
2023-06-16 10:52:28 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,10 +11,10 @@ int main(void)
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	{
 		putchar(alphabet);
-		for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
-		{
-			putchar(alphabet);
-		}
+	}
+	for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
+	{
+                        putchar(alphabet);
 	}
 	putchar('\n');
 	
