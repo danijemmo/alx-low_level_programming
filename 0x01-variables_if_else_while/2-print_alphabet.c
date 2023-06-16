d283a1d3750d11e7ec2afdcaf1a282d1503958e4
daniel jemmo <danieljemmo1@gmail.com>
2023-06-16 10:21:29 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (sucess);
+ */
+int main (void)
+{
+	char alphabet;
+	for(alphabet = 'A';alphabet <= 'Z';alphabet++)
+	{
+		putchar(alphabet);
+	}
+}
