8c57267cb6a23d426d62b86f7dc2d7d762919ee2
daniel jemmo <danieljemmo1@gmail.com>
2023-06-21 10:52:46 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (sucess);
+ */
+int main(void)
+{
+	for (a = 'a'; a <= z; a++)
+	{
+		_putchar(a + '0');
+	}
+	_putchar('\n');
+	return (0);
+}
