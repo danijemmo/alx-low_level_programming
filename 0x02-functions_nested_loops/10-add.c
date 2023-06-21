b2b13b32e8ca4548323e60ea9b3775d5d264cb22
daniel jemmo <danieljemmo1@gmail.com>
2023-06-21 14:19:27 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - ad
+ * @n1: number 1
+ * @n2: number 1
+ * Return: 0
+ */
+int add(int n1, int n2)
+{
+	result = n1 + nu2;
+
+	_putchar(result + '0');
+	return (result);
+}
