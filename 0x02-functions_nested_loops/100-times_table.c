ae2072bc38c7970109fd0ac3824b651e54282f78
daniel jemmo <danieljemmo1@gmail.com>
2023-06-21 15:10:17 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -20,7 +20,7 @@ void print_times_table(int n)
 				if (prod <= 99)
 					_putchar(' ');
 				if (prod <= 9)
-					putchar(' ');
+					_putchar(' ');
 				if (prod >= 100)
 				{
 					_putchar((prod / 100) + '0');
