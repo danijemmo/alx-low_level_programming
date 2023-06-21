e3d54ae8b0404514e579a8da577abf36e700daed
daniel jemmo <danieljemmo1@gmail.com>
2023-06-21 11:12:57 -0700

    print_alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alphabet x10
+ */
+void print_alphabet_x10(void)
+{
+	char a
+	int i
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a);
+	}
+	for (i = 1; i <= 10; i++)
+	{
+		_putchar(a);
+	}
+}
