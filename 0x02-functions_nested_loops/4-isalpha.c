084723aeea4f37a18f38fdf2bd18f841f9a1b2da
daniel jemmo <danieljemmo1@gmail.com>
2023-06-21 12:52:23 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _islower - check if the inpute is lowercase
+ * @c: int variable
+ * Return: 1 for lowercase 0 for upercase
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
